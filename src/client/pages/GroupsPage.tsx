11fecf331dfa9aab44d487c755432eb5d502d5de
Michael Muz <20muzm01@gmail.com>
2026-03-05 23:02:36 -0500

    extra touches

--- src/client/pages/GroupsPage.tsx
+++ src/client/pages/GroupsPage.tsx
@@ -10,8 +10,8 @@ export default function GroupsPage() {
   return (
     <Layout>
       <div className="flex items-center justify-between">
-        <h1>My Groups</h1>
-        <Button asChild size="sm">
+        <h1 className='font-bold text-lg'>My Groups</h1>
+        <Button asChild>
           <Link to="/groups/create">Create Group</Link>
         </Button>
       </div>
@@ -24,7 +24,7 @@ export default function GroupsPage() {
         <div className='flex flex-col gap-y-4 mt-4'>
           {groups.map((group) => (
             <Link to={`/groups/${group.id}`} key={group.id}>
-              <Card className='flex flex-col px-4 hover:bg-muted'>
+              <Card className='flex flex-col px-4 py-2 hover:bg-muted'>
                 <span>{group.name}</span>
                 <span className='text-sm text-muted-foreground'>{group.members.length} members</span>
               </Card>
