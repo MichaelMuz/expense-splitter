d36c2a4ddbb7c4d30d042a8e2a2f49e4cf2a76ce
Michael Muz <20muzm01@gmail.com>
2026-03-10 22:22:20 -0400

    cleaned up expense list

--- src/client/components/expenses/ExpenseList.tsx
+++ src/client/components/expenses/ExpenseList.tsx
@@ -4,7 +4,7 @@ import { useExpenses, useDeleteExpense } from '../../hooks/useExpenses';
 import { Loading } from '../layout/Loading';
 import { Card } from '../ui/card';
 import { Button } from '../ui/button';
-import { Pencil, Trash2 } from 'lucide-react';
+import { Trash2 } from 'lucide-react';
 
 export function ExpenseList({ groupId }: { groupId: string }) {
   const { data: expenses, isLoading, error } = useExpenses(groupId);
@@ -21,18 +21,19 @@ export function ExpenseList({ groupId }: { groupId: string }) {
   if (!expenses || expenses.length === 0) return <p>No expenses yet.</p>;
 
   return (
-    <ul>
+    <div className='flex flex-col gap-y-4 mt-4'>
       {expenses.map((expense) => (
-        <Card key={expense.id}>
-          {expense.name} — {formatCurrency(expense.totalAmount)}
-          <Button variant="outline" size="icon" onClick={() => handleDelete(expense.id)}>
+        <Card key={expense.id} className='flex items-center px-4 py-2 justify-between hover:bg-muted relative'>
+          <Link to={`/groups/${groupId}/expenses/${expense.id}/edit`} className='flex flex-col after:absolute after:inset-0'>
+            <div>{expense.name}</div>
+            <div className='text-sm text-muted-foreground'>{formatCurrency(expense.totalAmount)}</div>
+          </Link>
+          <Button variant="outline" size="icon" className='relative z-10' onClick={() => handleDelete(expense.id)}>
             <Trash2 />
-          </Button>
-          <Button variant="outline" size="icon" asChild>
-            <Link to={`/groups/${groupId}/expenses/${expense.id}/edit`} ><Pencil /></Link>
           </Button>
         </Card>
       ))
       }
-    </ul >);
+    </div >
+  );
 }
