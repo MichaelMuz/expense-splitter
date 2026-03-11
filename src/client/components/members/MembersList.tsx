aa8e1297dc2b54d91bd8ea522f7f2a2465dfe617
Michael Muz <20muzm01@gmail.com>
2026-03-10 23:21:38 -0400

    members list style

--- src/client/components/members/MembersList.tsx
+++ src/client/components/members/MembersList.tsx
@@ -2,6 +2,8 @@ import { Copy } from "lucide-react";
 import type { Group } from "@/shared/schemas/group";
 import { Button } from "../ui/button";
 import { toast } from "sonner";
+import { Badge } from "../ui/badge";
+import { Card } from "../ui/card";
 
 
 export function MembersList({ groupMembers, inviteCode }: { groupMembers: Group['members']; inviteCode: Group['inviteCode'] }) {
@@ -13,16 +15,18 @@ export function MembersList({ groupMembers, inviteCode }: { groupMembers: Group[
             Invite code: {inviteUrl}
             <Button variant="outline" size="icon" onClick={() => { navigator.clipboard.writeText(inviteUrl); toast("Copied!") }} > <Copy /></Button>
             <h2>Members</h2>
-            <ul>
+            <div className='flex flex-col gap-y-4 mt-4'>
                 {groupMembers.map((m) => (
-                    <li key={m.id}>
+                    <Card key={m.id} className='flex items-center px-4 py-2 justify-between'>
                         {m.name}
-                        {m.role === 'owner' ? ' (owner)' : ''}
-                        {!m.userId ? ' (virtual)' : ''}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-
+                        <div className='flex gap-x-2'>
+                            {!m.userId && <Badge variant='outline'>virtual</Badge>}
+                            {m.role === 'owner' && <Badge>owner</Badge>}
+                        </div>
+                    </Card>
+                ))
+                }
+            </div >
+        </div >
+    );
 }
