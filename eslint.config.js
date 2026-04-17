0633a315483cf9bb8fa210d3ec201d3657e66d2c
Michael Muz <20muzm01@gmail.com>
2026-04-16 20:23:38 -0400

    detect deprecations

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,15 @@ import reactHooks from 'eslint-plugin-react-hooks';
 
 export default tseslint.config(
   js.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
   {
     files: ['**/*.{ts,tsx}'],
     plugins: {
@@ -21,10 +29,11 @@ export default tseslint.config(
         'warn',
         { argsIgnorePattern: '^_' },
       ],
+      '@typescript-eslint/no-deprecated': 'warn',
     },
     settings: {
       react: {
-        version: 'detect',
+        version: '19',
       },
     },
   },
